010b21933689c3d46153f69fe5984d8bd1c0aa1c
annt <annt@gft.com>
2022-11-21 17:04:59 +0000

    done with if else

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,31 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import com.sun.jdi.IntegerValue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+
+        List<String> result = new ArrayList<>();
+        String aux = "";
+
+        for(int i = 1; i<n + 1; i++){
+
+            if(i % 3 == 0 && i % 5 == 0){
+                aux = "FizzBuzz";
+            }else if(i % 3 == 0){
+                aux = "Fizz";
+            }else if(i % 5 == 0){
+                aux = "Buzz";
+            }else{
+                aux = Integer.toString(i);
+            }
+
+            result.add(aux);
+        }
+
+        return result;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class SolutionTest {
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SolutionTest {
+
+    Solution solution;
+    int num;
+
+    List<String> result;
+
+    @BeforeEach
+    void setup(){
+        solution = new Solution();
+        result = new ArrayList<>();
+    }
+
+    @Test
+    public void given2returnListOfStrings(){
+        num = 2;
+        result.add("1");
+        result.add("2");
+
+        assertEquals(result, solution.fizzBuzz(num));
+    }
+
+    @Test
+    public void given3returnListOfStrings(){
+        num = 3;
+        result.add("1");
+        result.add("2");
+        result.add("Fizz");
+
+        assertEquals(result, solution.fizzBuzz(num));
+    }
+
+    @Test
+    public void given15returnListOfStrings(){
+        num = 15;
+        result.add("1");
+        result.add("2");
+        result.add("Fizz");
+        result.add("4");
+        result.add("Buzz");
+        result.add("Fizz");
+        result.add("7");
+        result.add("8");
+        result.add("Fizz");
+        result.add("Buzz");
+        result.add("11");
+        result.add("Fizz");
+        result.add("13");
+        result.add("14");
+        result.add("FizzBuzz");
+
+        assertEquals(result, solution.fizzBuzz(num));
+    }
 }
